0d96fa92bcb87eb54cda74db17f361e7dec59369
louiso1 <114828731+louiso1@users.noreply.github.com>
2024-04-28 00:32:53 +0000

    almost done

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,55 @@
+class PhotosController < ActionController::Base
+
+  def index
+    render({ :template => "/photo_templates/index" })
+  end
+
+  def show
+    photo_id = params.fetch("photo_id")
+    @photo = Photo.where({ :id => photo_id }).first
+    render({ :template => "/photo_templates/show" })
+  end
+
+  def create
+    p = Photo.new
+    p.image = params.fetch("input_image")
+    p.caption = params.fetch("input_caption")
+    owner = User.where({ :id => params.fetch("input_owner_id")}).first
+    p.owner_id = params.fetch("input_owner_id")
+    p.save
+    redirect_to("/photos")
+  end
+
+  def update
+    photo_id = params.fetch("id")
+    photo = Photo.where({ :id => photo_id }).first
+    photo.image = params.fetch("input_image")
+    photo.caption = params.fetch("input_caption")
+    photo.save
+    redirect_to("/photos/#{photo_id}")
+  end
+
+  def destroy
+    photo_id = params.fetch("id")
+    photo.destroy
+    redirect_to("/photos")
+  end
+
+  def add_comment
+    c = Comment.new
+
+    photo_id = params.fetch("input_photo_id")
+    c.photo_id = photo_id
+
+    author_id = params.fetch("input_author_id")
+    c.owner_id = author_id
+
+    comment = params.fetch("input_comment")
+    c.body = comment
+
+    c.save
+
+    redirect_to("/photos/#{photo_id}")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
  
+  # Homepage
   get("/", { :controller => "users", :action => "home" })
 
+  # Users
   get("/users", { :controller => "users", :action => "index" })
   post("/add_user", { :controller => "users", :action => "create" })
   get("/users/:username", { :controller => "users", :action => "show" })
   post("/update_user/:username", { :controller => "users", :action => "update" })
+
+  # Photos
+  get("/photos", { :controller => "photos", :action => "index" })
+  post("insert_photo_record", { :controller => "photos", :action => "create" })
+  get("/photos/:photo_id", { :controller => "photos", :action => "show" })
+  post("/update_photo/:id", { :controller => "photos", :action => "update" })
+  get("/delete/:id", { :controller => "photos", :action => "destroy" })
+  post("/add_comment", { :controller => "photos", :action => "add_comment" })
 
 end
